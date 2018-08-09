4dbc28bd31ab10c5359950c43c895ac7da378abd
SuzanneRashka <suzanne.rashka@gmail.com>
2018-08-09 18:12:47 -0500

    train times not working

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,59 +1,95 @@
-var config = {
-    apiKey: "AIzaSyCFrRLT1rQdbkUz0jjkehSIRAgiN-FJaLA",
-    authDomain: "trainscheduler-3d8be.firebaseapp.com",
-    databaseURL: "https://trainscheduler-3d8be.firebaseio.com",
-    projectId: "trainscheduler-3d8be",
-    storageBucket: "",
-    messagingSenderId: "760293447307"
-};
+$(document).ready(function () {
 
-firebase.initializeApp(config);
 
-// Create a variable to reference the database.
-var database = firebase.database();
+    var config = {
+        apiKey: "AIzaSyCFrRLT1rQdbkUz0jjkehSIRAgiN-FJaLA",
+        authDomain: "trainscheduler-3d8be.firebaseapp.com",
+        databaseURL: "https://trainscheduler-3d8be.firebaseio.com",
+        projectId: "trainscheduler-3d8be",
+        storageBucket: "",
+        messagingSenderId: "760293447307"
+    };
 
-// variables
+    firebase.initializeApp(config);
 
-// initial load
-database.ref().on("child_added", function (snapshot) {
+    // Create a variable to reference the database.
+    var database = firebase.database();
 
-    var snapVal = snapshot.val();
+    // variables
 
-    var tableRow = $("<tr>");
 
-    var nameEle = $("<td>").text(snapVal.trainName);
-    var destEle = $("<td>").text(snapVal.destination);
-    var timeEle = $("<td>").text(snapVal.trainTime);
-    var freqEle = $("<td>").text(snapVal.freq);
+    // initial load
+    database.ref().on("child_added", function (snapshot) {
 
-    //adding td elements to tableRow
+        var snapVal = snapshot.val();
 
-    tableRow
-        .append(nameEle)
-        .append(destEle)
-        .append(timeEle)
-        .append(freqEle);
+        var timeFrequency = snapVal.frequency;
+        var trainTime = snapVal.trainTime;
+        var trainTimeConverted = moment(trainTime, "HH:MM").subtract(1, "years");
+        console.log(trainTimeConverted);
 
-    $("#table-body").append(tableRow);
+        var currentTime = moment();
+        var currentTimeConverted = moment(currentTime).format("hh:mm");
+        console.log("currentTimeConverted = " + currentTimeConverted);
 
-});
+        var timeDiff = moment().diff(moment(trainTimeConverted), "minutes");
+        console.log("difference is: " + timeDiff);
 
-$("#submit").on("click", function () {
-    var formName = $("#train-name").val().trim();
-    var formDest = $("#destination").val().trim();
-    var formTime = $("#train-time").val().trim();
-    var formFreq = $("#freq").val().trim();
+        var timeModulus = timeDiff % timeFrequency;
+        console.log("timeModulus : " + timeModulus);
 
-    console.log(formName, formDest, formTime, formFreq);
+        var timeTillTrain = timeFrequency - timeModulus;
+        console.log("minutes til train" + timeTillTrain);
 
-    database.ref().push({
-        trainName: formName,
-        destination: formDest,
-        trainTime: formTime,
-        freq: formFreq
+        var nextTrain = moment().add(timeTillTrain, "minutes");
+        console.log("arrival time = " + moment(nextTrain).format("hh:mm"));
+
+        var nameEle = $("<td>").text(snapVal.trainName);
+        var destEle = $("<td>").text(snapVal.destination);
+        var timeEle = $("<td>").text(snapVal.trainTime);
+        var freqEle = $("<td>").text(snapVal.freq);
+        var etaEle = $("<td>") + timeTillTrain;
+        var nextTrainEle = $("<td>") + nextTrain;
+
+        //adding td elements to tableRow
+        var tableRow = $("<tr>");
+
+        tableRow
+            .append(nameEle)
+            .append(destEle)
+            .append(timeEle)
+            .append(freqEle)
+            .append(etaEle)
+            .append(nextTrainEle);
+
+
+        $("#table-body").append(tableRow);
+
     });
-    $("#train-name").val("");
-    $("#destination").val("");
-    $("#train-time").val("");
-    $("#freq").val("");
-});
+
+    $("#submit").on("click", function (event) {
+
+        event.preventDefault();
+
+        var formName = $("#train-name").val().trim();
+        var formDest = $("#destination").val().trim();
+        var formTime = $("#train-time").val().trim();
+        var formFreq = $("#freq").val().trim();
+        console.log(formName, formDest, formTime, formFreq);
+
+        database.ref().push({
+            trainName: formName,
+            destination: formDest,
+            trainTime: formTime,
+            freq: formFreq,
+        });
+
+        $("#train-name").val("");
+        $("#destination").val("");
+        $("#train-time").val("");
+        $("#freq").val("");
+
+    });
+
+
+}) // end document.ready
